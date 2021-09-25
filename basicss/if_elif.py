154650a7dfbdba28e659ffb7ad9ad75d47565326
Udit Agarwal <franklin130510@gmail.com>
2021-09-25 19:29:06 +0530

    elif

---
+++ basicss/if_elif.py
@@ -0,0 +1,11 @@
+weather = input("hows the weather today")
+if weather.lower() == "warm":
+    print("put on some light cloths when u get out")
+elif weather == "cold":
+    print("take the jacket with you")
+elif weather == "raining":
+    print("wear rain coat and bring some pakoda")
+elif weather == "humid":
+    print("to mai kya kru")
+else:
+    print("kehna kya chate ho")
--- basicss/if_else.py
+++ basicss/if_else.py
@@ -2,4 +2,9 @@ x = int(input("enter value of x:"))
 if x >= 15:
     print("x is greater than or equals to 15")
 else:
-    print("x is smaller than 15")
+    print("x is smaller than 15") 
+y = int(input("enter the value of y:"))
+if x + y > 50:
+    print("well done")
+else:
+    print("nope")
