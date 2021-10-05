5ce0796934b6ece3b86819068897b07599960161
Udit Agarwal <franklin130510@gmail.com>
2021-10-05 22:39:10 +0530

    string

---
+++ strings/count_every_alphabet.py
@@ -0,0 +1,8 @@
+from string import ascii_lowercase
+content = '''
+The black skimmer (Rynchops niger) is a tern-like seabird, one of three very similar birds species in the skimmer genus Rynchops in the gull family Laridae. It breeds in North and South America. Northern populations winter in the warmer waters of the Caribbean and the tropical and subtropical Pacific coasts, but the South American races make only shorter movements in response to annual floods which extend their feeding areas in the river shallows'''
+num_of_a = content.count('a')
+print(f'a occurs {num_of_a} times')
+
+in_counter = content.count('in')
+print(f''in' occurs {in_})
--- strings/searching_in_string.py
+++ strings/searching_in_string.py
@@ -1,16 +1,6 @@
-#  find
-#  index
-# msg = "Once upon a time, there was a kingdon."
-# idx = msg.find("time")
-# print("time" was found at at index {idx})
-# idx = msg.find("queen")
-# print(f''queen' was found at index {idx}')
-# if idx == -1:
-#     print(f''queen' was not found')
-# idxKing = msg.find("King")
-# print(""king" was found at index {idxKing}")
-# idxA = msg.find("a")
-# print("a")
 
 
 
+# 3/10/21
+idxKing = msg.index("King")
+print(f''king' found at index)
